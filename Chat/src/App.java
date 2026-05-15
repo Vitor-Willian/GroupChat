8cc7dcb2acfcd963465a4f56cfd424ab489d0605
Vitor-Willian <vitor.peneluppi@unifesp.br>
2026-05-15 14:18:26 -0300

    Debug da classe GroupChat e testes unitários no app

--- Chat/src/App.java
+++ Chat/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        GroupChat groupChat = new GroupChat();
+
+        User user1 = new User("Alfa");
+        User user2 = new User("Bravo");
+        User user3 = new User("Charlie");
+
+        groupChat.addUser(user1);
+        groupChat.addUser(user2);
+        groupChat.addUser(user3);
+
+        user1.sendMessage("Olá, pessoal!");
+        user2.sendMessage("Oi, Alfa!");
     }
 }
--- Chat/src/GroupChat.java
+++ Chat/src/GroupChat.java
@@ -1,8 +1,12 @@
-import java.util.List;
+import java.util.*;
 
 public class GroupChat implements Mediator {
     private List<User> users;
     
+    public GroupChat() {
+        users = new ArrayList<>();
+    }
+
     public void addUser(User user) {
         users.add(user);
         user.setMediator(this);
@@ -10,6 +14,7 @@ public class GroupChat implements Mediator {
 
     @Override
     public void sendMessageOperation(String message, User sender) {
+        System.out.println("\n// " + sender.getUsername() + " enviou: " + message + " //\n");
         for (User user : users) {
             if (user != sender) {
                 user.receiveMessage(message);
